539f654096fb211d32d4a520e6736701ae7c220e
Bececrazy <bececrazy2@gmail.com>
2022-03-08 02:22:12 -0400

    new commit

---
+++ gitCommit.sh
@@ -0,0 +1,11 @@
+TOKEN="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"
+USERNAME="Bececrazy"
+REPO="nodejsBlockChain"
+
+git init
+git add -A
+git commit -m "new commit"
+git branch -M main
+git remote remove origin
+git remote add origin https://$TOKEN@github.com/$USERNAME/$REPO.git
+git push -u origin main
