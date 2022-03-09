a9a07414efa2925fb5ca184d446a7877a59bae30
Bececrazy <bececrazy2@gmail.com>
2022-03-08 23:15:27 -0400

    new commit

--- commit.sh
+++ commit.sh
@@ -1,3 +1,15 @@
+' 
+	##### Heroku Commit #####
+git config --global user.email "bececrazy2@gmail.com"
+git config --global user.name "Bececrazy"
+
+git add .
+git commit -am "make it better"
+git push heroku master
+	##### Heroku Commit #####
+'
+
+' ##### Github Commit ##### '
 TOKEN="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"
 USERNAME="Bececrazy"
 REPO="18yPorn"
