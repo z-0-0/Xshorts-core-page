41ab2aa98a4cb9335b7aae0f9d4aee89f2a05480
Bececrazy <bececrazy2@gmail.com>
2022-03-08 23:14:07 -0400

    new commit

--- commit.sh
+++ commit.sh
@@ -1,7 +1,11 @@
-git config --global user.email "bececrazy2@gmail.com"
-git config --global user.name "Bececrazy"
+TOKEN="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"
+USERNAME="Bececrazy"
+REPO="18yPorn"
 
-
-git add .
-git commit -am "make it better"
-git push heroku master
+git init
+git add -A
+git commit -m "new commit"
+git branch -M main
+git remote remove origin
+git remote add origin https://$TOKEN@github.com/$USERNAME/$REPO.git
+git push -u origin main
--- update.js
+++ update.js
@@ -7,47 +7,29 @@ const url = require('url');
 const fs = require('fs');
 
 //TODO: String Normalization ################################################ //
-//https://store.externulls.com/webmasters/data.txt?days_back=10000&delimiter=%27|%27&secondary_delimiter=%27,%27&thumbs_number=1&fields=id,thumbs,title,duration,tags,people&thumb_params=ratio=16x9
+const slugify = str => { const map = {
+    'c' : 'ç|Ç','n' : 'ñ|Ñ',
+   	'e' : 'é|è|ê|ë|É|È|Ê|Ë',
+   	'i' : 'í|ì|î|ï|Í|Ì|Î|Ï',
+   	'u' : 'ú|ù|û|ü|Ú|Ù|Û|Ü',
+   	'o' : 'ó|ò|ô|õ|ö|Ó|Ò|Ô|Õ|Ö',
+   	'a' : 'á|à|ã|â|ä|À|Á|Ã|Â|Ä',
+	''	: ` |:|_|!|¡|¿|\\?|#|/|,|-|'|"|’`,
+};	for (var pattern in map) { 
+		str=str.replace( new RegExp(map[pattern],'g' ), pattern); 
+	}	return str.toLowerCase();
+}
+
 /*
-axios.get( 'https://store.externulls.com/webmasters/data.txt?days_back=10000&delimiter=%27|%27&secondary_delimiter=%27,%27&thumbs_number=7&fields=id,thumbs,title,duration,tags,people&thumb_params=size=320x240',{ responseType: 'stream' } )
-.then( (response)=>{
-
-	const file = readline.createInterface({
-	//	output: process.stdout,
-		input: response.data,
-		terminal: false
-	});
-	
-	file.on('line', async(line) => {
-		try{
-			let data = line.replace(/'/g,'').split('|');
-	
-			let video = {
-				id: data[0].slice(1),
-				name: data[2],
-				image: data[1].replace('https://','').split('?')[0],
-				duration: data[3],
-				category: data[4],
-				people: data[5],
-			};	//console.log( video );
-		
-			fs.promises.appendFile( './newdata',`${JSON.stringify(video)}\n` );
-			console.log('done');
-		} catch(e) {}
-	});
-
-}).catch( err=>console.log(err) )
-
-*/
-
 //https://store.externulls.com/facts/tag?slug=index&limit=100000000&offset=0
 //https://store.externulls.com/facts/tag?id=27173&limit=100000000&offset=0
-axios.get( 'https://store.externulls.com/facts/tag?id=27173&limit=100000000&offset=0',{ responseType: 'json' } )
-.then( (response)=>{
+axios.get( 'https://store.externulls.com/facts/tag?slug=index&limit=100&offset=0',{ responseType: 'json' } )
+.then( async(response)=>{
 		
 	let file_list = new Array();
 	
-	response.data.forEach( data=>{
+	for( var i in response.data ){
+		let data = response.data[i];
 		let video = new Object();
 		
 		video.category = new Array();
@@ -58,17 +40,52 @@ axios.get( 'https://store.externulls.com/facts/tag?id=27173&limit=100000000&offs
 		video.thumbs = data['fc_facts'][0]['fc_thumbs'][0];
 		video.image = `/thumbs-015.externulls.com/videos/${video.id}/${video.thumbs}.jpg/to.webp?size=320x180`
 		
-		for( var i in data['tags'] ){
-			let tag = data['tags'][i];
-			video.category.push( tag['tg_slug'] )			
-		}
+		data.tags.map( x=>{
+			video.category.push( slugify(x['tg_slug']) );
+			video.category.push( x['id'].toString() );
+		});	file_list.push(JSON.stringify(video));
 		
-		//console.log( video );
-		file_list.push(JSON.stringify(video));		
+		console.log( `done: ${i}/${response.data.length}` );
+	}
+	
+//	file_list = file_list.sort(() => Math.random()-0.5)
+	fs.writeFileSync('./data',file_list.join('\n'));
+
+}).catch(e=>console.log(e));
+*/
+
+axios.get( 'https://store.externulls.com/webmasters/data.txt?days_back=100000&delimiter=%27|%27&secondary_delimiter=%27,%27&thumbs_number=1&fields=duration,date,id,title,thumbs,brand,people,tags&thumb_params=ratio=16x9',{ responseType: 'stream' } )
+.then( response=>{
+		
+	let i=0;
+	let file_list = new Array();
+	
+	var lineaReader = readline.createInterface({
+		input: response.data,
+		debug: 'false'
+	});
+
+	lineaReader.on('line', (line)=>{ i++;
+		console.log( line )
+		let data = line.replace(/'/g,'').split('|');
+		let video = new Object();
+		
+		try{
+			video.name = data[3];
+			video.duration = data[0];
+			video.creation = data[1];
+			video.id = data[2].slice(1);
+			video.image = data[4].replace('https:/','');
+			video.category = (`${data[5]},${data[6]},${data[7]}`).split(',');
+		
+			file_list.push(JSON.stringify(video));
+		} catch(e) { console.log(e,line); }
+		
 	});
 	
-	if( d.search=='random' ){
-		data = data.sort(() => Math.random()-0.5)
-	}	fs.writeFileSync('./data',file_list.join('\n'));
-	
+	lineaReader.on('close',()=>{
+	//	file_list = file_list.sort(() => Math.random()-0.5)
+		fs.writeFileSync('./data',file_list.join('\n'));
+	});
+
 }).catch(e=>console.log(e));
