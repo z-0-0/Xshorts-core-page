970f74d90ba9e7f0a39cd419c8c603e70d524ff7
Bececrazy <bececrazy2@gmail.com>
2022-03-11 05:25:53 -0400

    new commit

--- main.js
+++ main.js
@@ -94,14 +94,6 @@ chunkheader = ( start,end,size,mimeType="text/plain" )=>{
 	};
 }
 
-//TODO: 404 Page Error XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX//
-_404_ = ()=>{ 
-	let url = `${path}/404.html`
-	if( fs.existsSync(url) )
-		return fs.readFileSync(`${path}/404.html`); 
-		return '404 not found';
-}
-
 //TODO: Server Started ###################################################### //
 router = (req,res)=>{ 
 	
@@ -112,12 +104,14 @@ router = (req,res)=>{
 	if( q.pathname=="/" ){
 		fs.readFile(`${path}/index.html`, (err,data)=>{
 			if (err) {
-				res.writeHead(404, header('text/html'));
-				return res.end( _404_() ); }
+				res.writeHead(301,{'Location':'/404'})
+				return res.end();
+			}
 			res.writeHead(200,header('text/html'));
 			res.write(data); res.end();
 		});
 	}
+	
 	else if( fs.existsSync(`${path}${q.pathname}.html`) ){
 		let data = fs.readFileSync(`${path}${q.pathname}.html`);
 		res.writeHead(200,header('text/html'));
@@ -259,8 +253,8 @@ router = (req,res)=>{
 			}
 		}
 		
-		res.writeHead(404, header('text/html'));
-		res.end( _404_() );
+		res.writeHead(301,{'Location':'/404'})
+		res.end();
 	}	
 	
 }
